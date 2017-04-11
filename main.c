910bc522081ccaaff6a8263bf8f47634cdaff1c0
Béuren Bechlin <beuren.bechlin@gmail.com>
2017-04-11 00:18:07 -0300

    Some rules added to lex file. Missing identifiers, literals, quotes and line counter. Implemented arguments to specify source file name.

--- main.c
+++ main.c
@@ -1,4 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* IMPORTANT: compile this before the lex file, so link all*/
 
-void main(){
-    
+#define FILE_OPT_STR 	"-file="
+#define FILE_OPT_NUMBER	6
+
+void main(int argc, char* argv[]){
+	int i;
+	for(i = 1; i < argc; i++){
+		// FILE NAME OPTION
+		if(strncmp(argv[i], FILE_OPT_STR, FILE_OPT_NUMBER) == 0){
+			yyin = fopen(&argv[i][FILE_OPT_NUMBER], "r");
+			printf("FILE_NAME : %s\n", &argv[i][FILE_OPT_NUMBER]);
+		}
+	}
+	i = yylex();
+	printf("%c %i\n", i, i);
 }
